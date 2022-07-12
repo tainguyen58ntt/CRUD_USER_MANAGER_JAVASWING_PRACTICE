ab9a898e3e456fde4f064c3d451ce5a7909770ec
tainguyen58ntt <tainguyen58ntt@gmail.com>
2022-07-12 14:24:45 +0700

    finish crud practice java swing

---
+++ src/main/java/com/mycompany/crud_user_manager_javaswing_practice/CRUD_USER_MANAGER_JAVASWING_PRACTICE.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.crud_user_manager_javaswing_practice;
+
+/**
+ *
+ * @author Admin
+ */
+public class CRUD_USER_MANAGER_JAVASWING_PRACTICE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
